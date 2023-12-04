fcab6d2d5718c7cec0c8eff6de6bf3f27c7e1aab
niqdevgit <107034072+niqdevgit@users.noreply.github.com>
2023-12-04 10:04:13 +0200

    backend "runs" frontend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log("hello")
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.static('dist'))
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
