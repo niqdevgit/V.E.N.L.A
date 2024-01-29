96fd8cd3003d9498ffbcb56750514f0d6166a28f
niqdevgit <107034072+niqdevgit@users.noreply.github.com>
2024-01-29 13:02:16 +0200

    commit

--- frontend/src/components/mainMenu.jsx
+++ frontend/src/components/mainMenu.jsx
@@ -1,17 +1,38 @@
+import MainTree from "./mainTree";
+
+
 // eslint-disable-next-line react/prop-types
 const MainMenu = ({user,setUser}) => {
   
-    const handleClick = () => {
+    const handleVisitorClick = () => {
         setUser('Vieras')
     }
+
+    const handleVisitorOutClick = () => {
+      setUser(null)
+  }
+
+
     console.log(user)
   return (
     <div className="main-menu">
       <h1>V.E.N.L.A</h1>
-      <p>Welcome, {user}!</p>
-      <p>Jotain lorem ipsumia mikä on sovelluksen idea.....</p>
-    <button><a href="/kirjaudu">Kirjaudu</a></button><button onClick={handleClick}>Käytä vieraana💀</button>
-      {/*if user is null, we suggest login. otherwise functional buttons here  */}
+      {user ? (
+                <div>
+                  <div style={{ display: 'flex', alignItems: 'center' }} >
+                  <p style={{ marginRight: '15px' }}>Hei,{user}!</p>  
+                  <button onClick={handleVisitorOutClick}>Kirjaudu ulos</button>
+                  </div>
+                    <MainTree />
+                    
+                </div>
+            ) : (
+                <div>
+                    <p>Jotain lorem ipsumia mikä on sovelluksen idea.....</p>
+                    <button><a href="/kirjaudu">Kirjaudu</a></button>
+                    <button onClick={handleVisitorClick}>Käytä vieraana💀</button>
+                </div>
+            )}
     </div>
   );
 };
---
+++ frontend/src/components/mainTree.jsx
@@ -0,0 +1,11 @@
+const MainTree = () => {
+    return (
+    <div>
+        <br></br>
+         Aloita ruuan valinta
+        <p>Katso tilastoja</p>
+    </div>
+    )
+}
+
+export default MainTree
