944e13f3eccfdbedad10dd10318beeb73237b530
niqdevgit <107034072+niqdevgit@users.noreply.github.com>
2024-01-29 15:55:24 +0200

    get operation done

--- frontend/src/components/analytics.jsx
+++ frontend/src/components/analytics.jsx
@@ -1,7 +1,39 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const Analytics = () => {
+    const [foods, setFoods] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/api/foods')
+                setFoods(response.data)
+            } catch (error) {
+                console.error('Error fetching food data:', error)
+            }
+        };
+
+        fetchData()
+    }, [])
+   
+
+   
+  
+
+
     return (
     <div>
         <p>Katso tilastoja</p>
+        <ul>
+            {foods.map(food => (
+                <li key={food.id}>
+                    <p>Food: {food.food}</p>
+                    <p>Date: {food.date}</p>
+                </li>
+            ))}
+        </ul>
+        
     </div>
     )
 }
