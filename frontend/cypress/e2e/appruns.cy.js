1a3d5ba11cbfe2daeda4d8d4b14ee8d0d2cee97c
niqdevgit <107034072+niqdevgit@users.noreply.github.com>
2024-02-02 09:56:57 +0200

    e2e fix

--- frontend/cypress/e2e/appruns.cy.js
+++ frontend/cypress/e2e/appruns.cy.js
@@ -1,14 +1,13 @@
 describe('Venla app', function() {
   beforeEach(function() {    
-    cy.visit('http://localhost:5173')
+    cy.visit('http://localhost:3001')
   })
 
   it('front page can be opened',  function() {
-    cy.visit('http://localhost:3001')
     cy.contains('V.E.N.L.A')
   })
 
-  it('login form can be opened', function() {
+  it('Visitor can login', function() {
     cy.contains('Käytä vieraana').click()
     cy.contains('Hei,vieras!')
   })
