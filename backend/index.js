cc0d2ceba711e61e7ab95f9e030244e3191de996
niqdevgit <107034072+niqdevgit@users.noreply.github.com>
2024-02-08 12:34:22 +0200

    backend refactor

--- backend/index.js
+++ backend/index.js
@@ -6,82 +6,30 @@ app.use(express.json())
 require('dotenv').config()
 app.use(cors())
 app.use(express.static('dist'))
-const Food = require('./models/food')
-const User = require('./models/user')
+const mongoose = require('mongoose')
+
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
-const jwt = require('jsonwebtoken')
-
-const getTokenFrom = request => {  
-  const authorization = request.get('authorization')  
-  if (authorization && authorization.startsWith('Bearer ')) {    
-    return authorization.replace('Bearer ', '')  
-  }  
-return null
-}
+const foodsRouter = require('./controllers/foods')
 
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/foods', foodsRouter)
 
-app.get('/api/foods', async (req, response) => {
-  try {
-    const decodedToken = jwt.verify(getTokenFrom(req),
-    process.env.JWT_SECRET)
-    //if token included, return "data of the request user"
-    if (decodedToken.username) {
-      const userFoods = await Food.find({ user: decodedToken.id })
-       return response.json({
-        data: userFoods,
-        status: "solo"
-      })
-    }
-  } 
- catch{
-  //else no token, return "global data"
-  Food.find({})
-    .then(foods => {
-      const formattedFoods = foods.map(food => ({
-        food: food.food,
-        date: food.date,
-        id: food.id
-      }));
 
-      response.json({
-        data: formattedFoods,
-        status: "global"
-      })
+
+mongoose.set('strictQuery', false)
+console.log('connecting to mongo')
+mongoose.connect(process.env.MONGODB_URI)
+
+  .then(result => {
+    console.log('connected to MongoDB')
   })
-}
-
-})
-
-app.post('/api/foods', async (req, res) => {
-  const body = req.body
-  const decodedToken = jwt.verify(getTokenFrom(req),
-  process.env.JWT_SECRET)
-
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })  
-  }  
-
-  const user = await User.findById(decodedToken.id)
-  
-  if (body.food === undefined) {
-    return res.status(400).json({ error: 'food missing' })
-  }
-
-  const food = new Food({
-    food: body.food,
-    date: body.date,
-    user: user._id
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
   })
 
-  const savedFood = await food.save()
-  user.foods = user.foods.concat(savedFood._id)
-  await user.save()
-  
-  res.json(savedFood)
-})
+
 
 app.get('/styles/default.css', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'styles', 'default.css'))
