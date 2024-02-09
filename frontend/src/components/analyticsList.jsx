515533b07282adc854ab9371eb2e2fab89343c44
niqdevgit <107034072+niqdevgit@users.noreply.github.com>
2024-02-09 16:43:24 +0200

    filter foods

--- frontend/src/components/analyticsList.jsx
+++ frontend/src/components/analyticsList.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 const AnalyticsList = ({foods}) => {
 
     const [sortByDate, setSortByDate] = useState(true)
+    const [foodsWithEmoji, setFoodsWithEmoji] = useState([])
+    const [selectedFilter, setSelectedFilter] = useState('default')
 
     const parseDate = (dateString) => {
         const [day, month, year] = dateString.split('.')
         return new Date(`${year}-${month}-${day}`)
     }
 
-    const sortedFoods = [...foods].sort((a, b) => {
+    const sortedFoods = [...foodsWithEmoji].sort((a, b) => {
         const dateA = parseDate(a.date)
         const dateB = parseDate(b.date)
 
@@ -21,17 +23,56 @@ const AnalyticsList = ({foods}) => {
         setSortByDate((prevSortByDate) => !prevSortByDate)
     }
 
+
+    useEffect(() => {
+        const addEmojiToFood = (foodName) => {
+            switch (foodName) {
+                case 'Pitsa':
+                    return '🍕'
+                case 'Ranskalaiset':
+                    return '🍟'
+                case 'Kebab':
+                    return '🌯'
+                default:
+                    return ''
+            }
+        }
+
+        const filteredFoods = selectedFilter !== 'default' ? foods.filter(food => food.food === selectedFilter) : foods
+
+        const foodsWithEmoji = filteredFoods.map(food => ({
+            ...food,
+            foodWithEmoji: food.food + ' ' + addEmojiToFood(food.food)
+        }));
+
+        setFoodsWithEmoji(foodsWithEmoji)
+    }, [foods, selectedFilter])
+
+    const handleFilterChange = (event) => {
+        setSelectedFilter(event.target.value)
+        console.log(event.target.value)
+    }
+
     return (
         <div>
+            <div className='flex-box'>
             <button onClick={toggleSortOrder}>
                 {sortByDate ?  'Vanhin ensin' : 'Uusin ensin'}
             </button>
+            <select id="selectFilter" onChange={handleFilterChange} value={selectedFilter}>
+                <option value="default">Ei filtteriä</option>
+                <option value="Pitsa">Pitsa</option> 
+                <option value="Ranskalaiset">Ranskalaiset</option>
+                <option value="Kebab">Kebab</option>
+            </select>
+            </div>
             <div className='scrollable-div'>
                 <ul>
                     {sortedFoods.map(food => (
-                        <div key={food.id}>
-                            <p>{food.food} {food.date}</p>
-                        </div>
+                         <div key={food.id} className="food-item">
+                         <p className="food-name">{food.foodWithEmoji}</p>
+                         <p className="food-date">{food.date}</p>
+                     </div>
                     ))}
                 </ul>
             </div>
